12553cd0d2aa06ee049325857a3dd2e47020dedb
terufumi1122 <terufumi1122@gmail.com>
2020-11-15 12:22:17 +0900

    add: React側で新規親作成機能を実装

--- frontend/src/components/molecules/PostForm.tsx
+++ frontend/src/components/molecules/PostForm.tsx
@@ -1,19 +1,177 @@
-import { Button, FormHelperText } from '@material-ui/core';
-import FormControl from '@material-ui/core/FormControl/FormControl';
-import InputLabel from '@material-ui/core/InputLabel/InputLabel';
-import { Input } from '@material-ui/icons';
-import React from 'react';
+import React , { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-interface Prop {
-  columns: any[],
-  data: any[],
-}
+import axios from 'axios';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker,} from '@material-ui/pickers';
 
-function PostForm() {
-  return (
-    <>
-    </>
+const ENDPOINT = 'http://localhost:8080/api/parents';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function PostForm() {
+  const classes = useStyles();
+
+  const [email, setEmail] = useState("")
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const [gender, setGender] = useState("0")
+  const [birthday, setBirthday] = useState<Date | null>(
+    new Date('1984/12/11')
   )
-}
 
-export default PostForm
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGender((event.target as HTMLInputElement).value);
+  }
+
+  const handleDateChange = (date: Date | null) => {
+    setBirthday(date);
+  };
+
+  const createParent = async () => {
+    const reqBody = {
+      email: email,
+      name: name,
+      password: password,
+      gender: gender,
+      birthday: birthday
+    };
+    await axios.post(ENDPOINT, reqBody);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Create New Parent
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={createParent}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                autoComplete="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Gender</FormLabel>
+                <RadioGroup row aria-label="gender" name="gender" value={gender} onChange={handleChange}>
+                  <FormControlLabel value="2" control={<Radio />} label="Female" />
+                  <FormControlLabel value="1" control={<Radio />} label="Male" />
+                  <FormControlLabel value="0" control={<Radio />} label="Other" />
+                </RadioGroup>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="space-around">
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="yyyy/MM/dd"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="Birthday"
+                    value={birthday}
+                    onChange={handleDateChange}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                  />
+                </Grid>
+              </MuiPickersUtilsProvider>
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Create New Parent
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
--- frontend/src/components/organisms/Parents.tsx
+++ frontend/src/components/organisms/Parents.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Table from '../molecules/Table';
+import PostForm from '../molecules/PostForm';
 
 const ENDPOINT = 'http://localhost:8080/api/parents';
 
@@ -27,6 +28,7 @@ export default function Parents() {
     // deleteFlg: "",
   }]);
 
+  // ページ読み込み時に親一覧を取得する
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(ENDPOINT);
@@ -37,6 +39,7 @@ export default function Parents() {
 
   return (
     <>
+      <PostForm></PostForm>
       <Table columns={columns} data={parents} />
     </>
   );
