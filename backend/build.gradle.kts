e4bc04aed75ef129078333ac71152d1768b8b2c9
terufumi1122 <terufumi1122@gmail.com>
2020-11-28 08:02:14 +0900

    config: testImplementation("com.ninja-squad:springmockk:2.0.1")

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
+	testImplementation("com.ninja-squad:springmockk:2.0.1")
 }
 
 tasks.withType<Test> {
